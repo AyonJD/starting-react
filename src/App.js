79425635680f127e16d4aa803871df2080fa6765
Ayon <ayonjodder177@gmail.com>
2022-03-16 19:53:01 +0600

    Pass dynamic data to components, props with passing attribute while calling the function

--- src/App.js
+++ src/App.js
@@ -5,18 +5,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <CreateDiv></CreateDiv>
+        {/* <CreateDiv></CreateDiv> */}
+
+        <Dynamic header = "It's the header passed by attribute" paragraph = "It's a paragraph passed by attribute"></Dynamic>
       </header>
     </div>
   );
 }
 export default App;
 
-const CreateDiv = () => {
+
+//Add some element and styles with JSX
+
+/* const CreateDiv = () => {
   return (
     <div>
       <h1>It's a header</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, in sit! Aliquid exercitationem amet consequuntur voluptates blanditiis natus iure quia, dolore aut nihil eaque? Inventore in magnam tenetur id nisi veritatis iste rem eveniet cum? Distinctio excepturi fugiat cupiditate atque, ea unde sint ex odit nostrum sequi! Ducimus, animi pariatur.</p>
+    </div>
+  )
+} */
+
+
+//Pass dynamic data to components, props with passing attribute while calling the function
+
+const Dynamic = (props) => {
+
+  return (
+    <div>
+      <h1 style={{color: 'red'}}>{ props.header }</h1>
+      <p style={{color: 'green', backgroundColor: 'hotpink', padding: '20px'}}>{ props.paragraph }</p>
     </div>
   )
 }
